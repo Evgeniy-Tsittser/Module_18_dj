7239944c0c3c7db350d401dc55eb2cef59fcbc96
Evgeniy-Tsittser <evgenc1980@mail.ru>
2024-10-30 16:05:46 +0700

    изменения по заданию 2. модуля 18

--- UrbanDjango/task2/urls.py
+++ UrbanDjango/task2/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from . import views
 from .views import func_repres, class_repres
 
 urlpatterns = [
     path('func/', func_repres),
-    path('class/', class_repres),
+    path('class/', class_repres.as_view()),
 ]
--- UrbanDjango/task2/views.py
+++ UrbanDjango/task2/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
 
 def func_repres(request):
     return render(request, 'second_task/func_repres.html')
 
-def class_repres(request):
-    return render(request, 'second_task/class_repres.html')
+class class_repres(TemplateView):
+    template_name = 'second_task/class_repres.html'
